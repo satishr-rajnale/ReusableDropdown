946678c022ee742a84cb004b68234037cf98e621
Rajnale <p50039553@capita.co.uk>
2021-05-03 16:05:22 +0100

    updated changes for reusable component

--- src/components/ReusableDropdown.js
+++ src/components/ReusableDropdown.js
@@ -1,27 +1,6 @@
 import React from "react";
 import Select, { components } from "react-select";
 
-const colourOptions = [
-  {
-    value: "red",
-    label: "Red",
-    description:
-      "Is there any other way using bootstrap or material UI I can achieve this. I am using react select npm package for dropdown list .you can find live link and code below"
-  },
-  {
-    value: "green",
-    label: "Green",
-    description:
-      'Message to display in the menu when there are no options and isLoading is true. By default it is "Loading..."'
-  },
-  {
-    value: "blue",
-    label: "Blue",
-    description:
-      "Loading indicator to be displayed in the Indicators Container when isLoading is true. By default it is three dots."
-  }
-];
-
 const SelectDrop = (props) => {
   console.log(props);
   const colourStyles = {
@@ -81,7 +60,6 @@ const SelectDrop = (props) => {
 
 
   };
-
   const Option = (OptionProps) => {
     return (
       <components.Option {...OptionProps}>
@@ -96,9 +74,9 @@ const SelectDrop = (props) => {
 
   return <div>
     <Select
-      defaultValue={colourOptions[1]}
+      defaultValue={props.data.data[0]}
       label="Single select"
-      options={colourOptions}
+      options={props.data.data}
       styles={colourStyles}
       components={{
         Option
--- src/index.js
+++ src/index.js
@@ -4,24 +4,60 @@ import ReactDOM from "react-dom";
 import SelectDropdown from "./components/ReusableDropdown";
 import "./styles.css";
 
+const menulist = [
+  {
+    value: "Hopper",
+    label: "Hopper",
+    description:
+      "Grace Hopper was an American computer scientist and US Nevy near rear admiral."
+  },
+  {
+    value: "Holberton",
+    label: "Holberton",
+    description:
+      "Frances Elizabeth Holberton was one the programmers in the world."
+  },
+  {
+    value: "Teitelbaum",
+    label: "Teitelbaum",
+    description:
+      "Ruth Teitelbaum was one of the first computer programmers int the world."
+  }
+];
+
+const normalList = [
+  {
+    value: "Hopper",
+    label: "Hopper",
+  },
+  {
+    value: "Holberton",
+    label: "Holberton",
+   },
+  {
+    value: "Teitelbaum",
+    label: "Teitelbaum",
+   }
+];
+
 function App() {
   return (
     <div>
-      
+
       <div className="divWhite">
         <div Style="display: inline-block;margin-left:50px;margin-top:50px">
-          <SelectDropdown data={{ theme: 'white', description: false }} />
+          <SelectDropdown data={{ theme: 'white', description: false, data: normalList }} />
         </div>
         <div Style="display: inline-block;margin-left:100px">
-          <SelectDropdown data={{ theme: 'white', description: true }} />
+          <SelectDropdown data={{ theme: 'white', description: true, data: menulist }} />
         </div>
       </div>
       <div className="divBlack">
         <div Style="display: inline-block;margin-left:50px;margin-top:50px">
-          <SelectDropdown data={{ theme: 'black', description: false }} />
+          <SelectDropdown data={{ theme: 'black', description: false, data: normalList }} />
         </div>
         <div Style="display: inline-block;margin-left:100px">
-          <SelectDropdown data={{ theme: 'black', description: true }} />
+          <SelectDropdown data={{ theme: 'black', description: true, data: menulist }} />
         </div>
       </div>
     </div>
